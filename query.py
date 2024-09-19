d1501545f16ad091bdcac312e61b52ab7fc067cb
AndersonChaves <andersonchaves.net@gmail.com>
2024-09-19 17:47:57 -0300

    Changes in query

--- query.py
+++ query.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Connect to the SQLite database
-conn = sqlite3.connect('output/Q7-2015-random.db')
+conn = sqlite3.connect('output/query-results/best_of_all/Q5-2015-best_of_all.db')
 cursor = conn.cursor()
 
 # Define the SQL query
@@ -24,8 +24,14 @@ cursor = conn.cursor()
 
 
 sql_query = """
-SELECT iteration, configuration, AVG(error) from exp
+SELECT configuration, AVG(error) as avg_error from exp
+GROUP BY configuration
 """
+
+#sql_query = """
+# SELECT t.configuration, min(t.avg_error) from( SELECT configuration, AVG(error) as avg_error from exp
+# GROUP BY configuration) t
+#"""
 
 # Execute the query
 cursor.execute(sql_query)
@@ -46,8 +52,10 @@ conn.close()
 # conn = sqlite3.connect('output/best_of_all/Q7-2015-best_of_all.db')
 # cursor = conn.cursor()
 
-
 # sql_query = """
 # SELECT t.configuration, min(t.avg_error) from( SELECT configuration, AVG(error) as avg_error from exp
 # GROUP BY configuration) t
 # """
+
+#average dbs
+#SELECT iteration, configuration, AVG(error) from exp
